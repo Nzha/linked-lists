a99867d7284c6aebb26ed29b5dcc65910d19db00
Adrien <adrien.biz@gmail.com>
2023-01-22 13:00:39 +0400

    Add pop function and clean up code

--- linkedList.js
+++ linkedList.js
@@ -20,15 +20,13 @@ const LinkedList = () => {
   };
 
   const size = () => {
-    if (head !== null) {
-      let count = 1;
-      current = head;
-      while (current.next !== null) {
-        count += 1;
-        current = current.next;
-      }
-      return count;
+    let count = 1;
+    let current = head;
+    while (current.next !== null) {
+      count += 1;
+      current = current.next;
     }
+    return count;
   };
 
   const getHead = () => {
@@ -36,37 +34,30 @@ const LinkedList = () => {
   };
 
   const tail = () => {
-    if (head === null) {
-      return 'Empty list';
-    } else {
-      current = head;
-      while (current.next !== null) {
-        current = current.next;
-      }
-      return current;
+    let current = head;
+    while (current.next) {
+      current = current.next;
     }
+    return current;
   };
 
   const at = (index) => {
-    if (head !== null) {
-      let count = 0;
-      current = head;
-      while (count < index) {
-        current = current.next;
-        count += 1;
-      }
-      return current;
+    let count = 0;
+    let current = head;
+    while (count < index) {
+      current = current.next;
+      count += 1;
     }
+    return current;
   };
 
   const pop = () => {
-    if (head !== null) {
-      let current = head;
-      while (current.next !== null) {
-        current = current.next;
-      }
-      current = null;
+    let current = head;
+    while (current.next.next) {
+      current = current.next;
     }
+    current.next = null;
+    return current;
   };
 
   return { prepend, append, size, getHead, tail, at, pop };
@@ -82,10 +73,10 @@ list.append(6);
 list.prepend(3);
 list.prepend(1);
 list.append(7);
-// list.pop();
+list.pop();
 
 // 1-> 3-> 5 -> 6 -> 7
 
 // console.log(list.size());
 // console.log(list.tail());
-console.log(list.at(4));
+console.log(list.at(3));
